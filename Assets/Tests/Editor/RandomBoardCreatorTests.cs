1b1d88da203aa5be630bdaf977eb648ccb127095
Slawomir Grabowski <grabowski@quickturnstudio.com>
2020-05-31 13:00:18 +0200

    Setup Unit Test environment

---
+++ Assets/Tests/Editor/RandomBoardCreatorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using NSubstitute;
+
+namespace LocalModel.Tests
+{
+    public class RandomBoardCreatorTests
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            var randomDevice = Substitute.For<IRandomTypeProvider>();
+            Assert.IsTrue(true);
+        }
+    }
+
+} // namespace LocalModel.Tests
